0d0ebe65caff35b46e1f61d45a1729d028aeeccc
Anna <annkulakova96@gmail.com>
2021-04-01 16:44:02 +0300

    refactoring for dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,39 +2,38 @@ import * as React from "react";
 import styles from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
 
+export const DialogItem = (props) => {
+  let path = "/dialogs/" + props.id;
+  return (
+    <div className={styles.name + " " + styles.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+    </div>
+  )
+};
+
+export const Message = (props) => {
+  return (
+    <div className={styles.message}>{props.message}</div>
+  )
+};
+
 export const Dialogs = (props) => (
   <div className={styles.contacts}>
     <div className={styles.dialogs}>
-      <div className={styles.name + " " + styles.active}>
-        <NavLink to="/dialogs/1">Pasha</NavLink>
-      </div>
-      <div className={styles.name}>
-        <NavLink to="/dialogs/2">John</NavLink>
-      </div>
-      <div className={styles.name}>
-        <NavLink to="/dialogs/3">Mark</NavLink>
-      </div>
-      <div className={styles.name}>
-        <NavLink to="/dialogs/4">Alex</NavLink>
-      </div>
-      <div className={styles.name}>
-        <NavLink to="/dialogs/5">Robert</NavLink>
-      </div>
-      <div className={styles.name}>
-        <NavLink to="/dialogs/6">Sasha</NavLink>
-      </div>
+      <DialogItem name="Pasha" id="1" />
+      <DialogItem name="John" id="2" />
+      <DialogItem name="Mark" id="3" />
+      <DialogItem name="Alex" id="4" />
+      <DialogItem name="Robert" id="5" />
+      <DialogItem name="Sasha" id="6" />
     </div>
 
     <div className={styles.messages}>
-      <div className={styles.message}>Hey, Pasha, what`s up?</div>
-      <div className={styles.message}>Hi, Anny! I`m okay, how about u?</div>
-      <div className={styles.message}>
-        I`m good, just start learning JavaScript:3
-      </div>
-      <div className={styles.message}>
-        Wow it`s so cool! I could help you with it!
-      </div>
-      <div className={styles.message}>Yea, thanks:3 It will be great!</div>
+      <Message message="Hey, Pasha, what`s up?" />
+      <Message message="Hi, Anny! I`m okay, how about u?" />
+      <Message message="I`m good, just start learning JavaScript:3" />
+      <Message message="Wow it`s so cool! I could help you with it!" />
+      <Message message="Yea, thanks:3 It will be great!" />
     </div>
   </div>
 );
