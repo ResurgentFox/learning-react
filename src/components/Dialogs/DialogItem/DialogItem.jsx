9cc33bf328c538b1de69483bfe4a801480a804ad
Anna <annkulakova96@gmail.com>
2021-04-13 21:19:28 +0300

    new styles for Dialogs

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -5,8 +5,12 @@ import { NavLink } from "react-router-dom";
 export const DialogItem = (props) => {
   const path = "/dialogs/" + props.id;
   return (
-    <div className={styles.name + " " + styles.active}>
+    <div className={styles.friendsList}>
+      <div className={styles.avaFriend}></div>
+      <div className={styles.name + " " + styles.active}>
       <NavLink to={path}>{props.name}</NavLink>
+      </div>
+      <div className={styles.time}>16:42</div>
     </div>
   );
 };
