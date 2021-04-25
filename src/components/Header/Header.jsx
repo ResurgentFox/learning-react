5aa10c125dc386e04f24c5a39d4b40a58708718f
Anna <annkulakova96@gmail.com>
2021-04-25 21:00:08 +0300

    changed logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import styles from "./Header.module.css";
+import logo from "./logo/logo.png";
 
 export const Header = () => (
   <header className={styles.header}>
-    <div className={styles.logo}>{"{FIND<3R}"}</div>
+    <div className={styles.logo}>
+    <img className={styles.logo} alt={"logo"} src={logo}></img>
+    </div>
   </header>
 );
